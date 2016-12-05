7e773915e3c6e2565d2b36e006e6182ad94e73be
Guillaume Dorion-Racine <gdorion@gmail.com>
2016-12-04 23:56:11 -0500

    add day 4 part 2

--- day4.py
+++ day4.py
@@ -11,13 +11,31 @@ def validate(string, checksum):
             s_registry[c] = 1
 
     sorted_s = [v for v in sorted(s_registry.iteritems(), key=lambda(k, v): (-v, k))]
-    
+
     for i in range(len(checksum)):
         checksumLetter = checksum[i]
         roomLetter = sorted_s[i][0]
         if checksumLetter != roomLetter:
             return False
     return True
+
+def decrypt(c):
+    if c == 'z':
+        c = 'a'
+    elif c == 'Z':
+        c = 'A'
+    elif c != ' ':
+        c = chr(ord(c) + 1)
+    return c
+
+def rotate(string, count):
+    result = ""
+    for c in string:
+        newC = c
+        for i in range(count):
+            newC = decrypt(newC)
+        result += newC
+    return result
 
 sum = 0
 with open('day4.txt') as f:
@@ -32,7 +50,16 @@ with open('day4.txt') as f:
         lettersSections.pop(len(lettersSections)-1)
         rawString = "".join(lettersSections)
 
+        # part1
         if validate(rawString, checksum):
             sum = sum + int(sectorId)
+
+        lettersSections = row.split('[')[0].split('-')
+        lettersSections.pop(len(lettersSections)-1)
+        roomName = " ".join(lettersSections)
+        decrypted = rotate(roomName, int(sectorId))
+        if "north" in decrypted:
+            print decrypted
+            print sectorId
 
 print sum
